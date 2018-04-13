78642de5adc85f5d5314fed3307f0fd2e9739515
BunnyRoy <hbk316roy@gmail.com>
2018-04-13 21:20:03 +0530

    Committing -the complete project.

---
+++ Curve/Controllers/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Curve.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult MainHome()
+        {
+            return View();
+        }
+        public ActionResult RedirectToContent()
+        {
+            return PartialView("MainNew",null);// View();
+        }
+        
+    }
+}
---
+++ Curve/Scripts/CommonCurve.js
@@ -0,0 +1,50 @@
+﻿var MainCureve = {
+
+    OpenPopUp: function (element) {
+        debugger
+        $.ajax(
+            {
+                type: "GET",
+                url: "/Main/RedirectToContent",
+                contentType: "application/json; charset=utf-8",
+                cache: false,
+                data: {},
+                success: function (html) {
+                    debugger;
+                    $("#ModalContent").append(html);
+                    $('#myModal').modal('show');
+                },
+                error: function () {
+                    debugger;
+                    alert("Dynamic content load failed.");
+                }
+            });
+    },
+
+    OnLeave: function (element) {
+        debugger;
+        var txtNameValue = document.getElementById("txtName").value;
+        if (txtNameValue.length == 0)
+            document.getElementById("txtName").style.borderBottomColor = "red";
+        else
+            document.getElementById("txtName").style.borderBottomColor = "";
+    }
+    ,
+    ClearAll: function () {
+        debugger;
+        var arrelements = document.forms["FormMain"];
+        for (var i = 0; i < arrelements.length; i++) {
+            if (arrelements[i].type == "text") {
+                arrelements[i].value = "";
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+};
